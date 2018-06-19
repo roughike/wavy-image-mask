4b5b7fb9165a4c5a2e2ac112b5ec1d949449d3fe
Iiro Krankka <iiro.krankka@codemate.com>
2018-06-19 20:00:29 +0300

    Omitting new keywords, adding smoke test and Travis config.

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:wavy_image_mask/wavy_header_image.dart';
 
-void main() {
-  runApp(new MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Wavy image mask',
-      theme: new ThemeData(
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new WavyHeaderImage(),
+    return Scaffold(
+      body: WavyHeaderImage(),
     );
   }
 }
--- lib/wavy_header_image.dart
+++ lib/wavy_header_image.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 class WavyHeaderImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new ClipPath(
-      child: new Image.asset('images/coffee_header.jpeg'),
-      clipper: new BottomWaveClipper(),
+    return ClipPath(
+      child: Image.asset('images/coffee_header.jpeg'),
+      clipper: BottomWaveClipper(),
     );
   }
 }
@@ -13,17 +13,17 @@ class WavyHeaderImage extends StatelessWidget {
 class BottomWaveClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-    Path path = new Path();
+    Path path = Path();
     path.lineTo(0.0, size.height - 20);
 
-    var firstControlPoint = new Offset(size.width / 4, size.height);
-    var firstEndPoint = new Offset(size.width / 2.25, size.height - 30.0);
+    var firstControlPoint = Offset(size.width / 4, size.height);
+    var firstEndPoint = Offset(size.width / 2.25, size.height - 30.0);
     path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
         firstEndPoint.dx, firstEndPoint.dy);
 
     var secondControlPoint =
-        new Offset(size.width - (size.width / 3.25), size.height - 65);
-    var secondEndPoint = new Offset(size.width, size.height - 40);
+        Offset(size.width - (size.width / 3.25), size.height - 65);
+    var secondEndPoint = Offset(size.width, size.height - 40);
     path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
         secondEndPoint.dx, secondEndPoint.dy);
 
